b5a0c025be5e14022bb6cc8aaa5998a910082c63
hdk34bdj <rvdk2134jrj@gmail.com>
2025-07-31 18:59:24 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "lucky~fJMUWLQK#Lpqjni-XMMNKibJCp4Z0wQUo35ha64j0LXhIIfsmuak",
+SESSION_ID: process.env.SESSION_ID || "lucky~vJF0TQLJ#atbo_g2R8GcHdxoNJ06SKBDiQcB8UpwcuS4ctHxkbPY",
 // add your Session Id make sure it starts with lucky~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
@@ -68,7 +68,7 @@ LINK_WHITELIST: "youtube.com,github.com",
           DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 
-          OWNER_NUMBER: process.env.OWNER_NUMBER || "212604-539484",
+          OWNER_NUMBER: process.env.OWNER_NUMBER || "212680821981",
 // add your bot owner number
 
 OWNER_NAME: process.env.OWNER_NAME || "Luck212",
